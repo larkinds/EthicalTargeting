7dd9a086271129f921eef962e7b16432ef6c6a1b
Ali-Doggaz <charfedine611@gmail.com>
2021-04-03 20:44:28 +0100

    Added the extension init

--- src/helper/updateState.js
+++
@@ -1,9 +0,0 @@
-function updateState(category, newItems, setUser) {
-  if (newItems.length === 0) newItems = [''];
-  setUser((user) => ({
-    ...user,
-    [category]: newItems,
-  }));
-}
-
-export default updateState;
