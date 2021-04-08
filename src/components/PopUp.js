17d777c36028142f35b66b04b3673a70dc7c1d48
Larkin <larkindsmith@gmail.com>
2021-04-07 22:03:51 -0400

    working toggle

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -1,4 +1,9 @@
+import { Switch } from '@headlessui/react';
+import { useState } from 'react';
+
 function PopUp() {
+  let [toggle, setToggle] = useState(false);
+
   return (
     <div className="bg-white h-screen flex flex-col">
       <div className="bg-gray-800 h-24 w-auto flex justify-between content-center">
@@ -30,21 +35,27 @@ function PopUp() {
       </div>
       <div className="w-auto h-full border-2 flex flex-col justify-center">
         <div className="bg-gray-50 border-2 self-center w-72 h-auto p-6 m-5">
-          <div>
-            <h1>Do Not Track This Website</h1>
-            <input type="checkbox" />
-            <label>
-              <span id="on"></span>
-            </label>
-            <label>
-              <span id="off"></span>
-            </label>
+          <div className="flex justify-between items-center px-4 py-2">
+            <h1>Do Not Track Website:</h1>
+            {/* switch container */}
+            <Switch
+              onChange={() => setToggle(!toggle)}
+              className={`flex items-center w-14 h-9 p-1 flex-shrink-0 rounded-full ease-in-out cursor-pointer focus:outline-none ${
+                toggle ? 'bg-green-700' : 'bg-gray-100'
+              }`}
+            >
+              <span
+                className={`bg-white w-7 h-7 rounded-full shadow-md transition duration-200 transform  ${
+                  toggle ? 'translate-x-5' : 'translate-x-0'
+                }`}
+              />
+            </Switch>
           </div>
         </div>
         <div className="bg-gray-50 border-2 self-center w-72 h-auto p-6 m-5 flex flex-col">
           <h1>Do Not Track Mode</h1>
           <div>
-            <input className="" type="text" placeholder="30" />
+            <input className="w-" type="text" placeholder="30" />
             <label>Minutes</label>
           </div>
           <button className="bg-green-700 rounded text-white p-2 m-2">
