5e167452b12c609d7c3f0acc8d63e3a52d75c2ce
Ali-Doggaz <charfedine611@gmail.com>
2021-04-09 13:06:50 +0100

    Cleaned code

--- public/backgroundCrawler.js
+++ public/backgroundCrawler.js
@@ -1,18 +1,20 @@
 /* eslint-disable no-undef */
-//at installation
+
+let dataCollectionAutorizedBool = true; // Set to true if the user authorizes the data collection
+
+// at installation, initialize the user profile 
 chrome.runtime.onInstalled.addListener((details) => {
     const reason = details.reason;
-    console.log(reason);
-    if (reason === "install" || reason ==="update"){
+    if (reason === "install"){
       //NOTE: need to update this with the user's info
       var userInfo = {
-        'profileName':'Prabal',
-        'age':'21',
-        'gender':'Male',
-        'occupation':'Student',
-        'region':'United States',
-        'interests':{'Pokemon':{'name': 'pokemon', 'weigth':1}, 'Adventure':{'name':'adventure', 'weight':2}, 'colours':{'name':'colours', 'weight':3}},
-        'dislikes':{'Racism':{'name': 'racism', 'weight': 5},'Serious People':{'name': 'invalidation', 'weight': 2}}
+        'profileName':'N/A',
+        'age':'N/A',
+        'gender':'N/A',
+        'occupation':'N/A',
+        'region':'N/A',
+        'interests':{},
+        'dislikes':{}
         }
       chrome.storage.sync.set(userInfo);
     }
@@ -20,90 +22,42 @@ chrome.runtime.onInstalled.addListener((details) => {
   
   const tabListener = function(tabId, tab){
     //update interests of the user:
-    let keywordsDict = {};
     let script = {file: `getMeta.js`};
     chrome.tabs.executeScript(tabId, script)
   }
   
-  //at new tab creation, with an exception to the "new tab" page
-  chrome.tabs.onCreated.addListener((tab)=>{
-    tabListener(tab.id, tab)
-    console.log("onCreated called")
-  });
-  
-  
-  //When a updates a certain tab
+  //When accessing a new website, collects the metatags and update the user's profile
   chrome.tabs.onUpdated.addListener(function
       (tabId, changeInfo, tab) {
         //if the user visits a new page
-        if (changeInfo.url) {
+        if (changeInfo.url && dataCollectionAutorizedBool) {
           tabListener(tabId, tab)
-          console.log("onUpdate called")
         }
       }
   );
   
-  function saveState(state) {
-    // Get a value saved in a form.
-    // Check that there's some code there.
-    if (!state) {
-      console.log('Error: No value specified');
-      return;
-    }
-    // Save it using the Chrome extension storage API.
-    chrome.storage.sync.set({'userInfo': state.userInfo}, function() {
-      // Notify that we saved.
-      console.log('Settings saved');
-    });
-  }
   
-  async function getState() {
-      let result = chrome.storage.sync.get(['userInfo'], function(result){
-        console.log(result);
-      });
-  }
-  
-  function removeState(){
-    chrome.storage.sync.remove(['userInfo'], (err)=>{
-      console.log(err);
-    })
-  }
-  
-  async function updateStringInfo(key, value)
-  {
-    //get the state
-    chrome.storage.sync.get(['userInfo'], function(result){
-      chrome.storage.sync.remove(['userInfo'], (err)=>{
-        
-        result.userInfo.name = value
-        console.log(result)
-  
-        saveState(result);
-      })
-    });
-  }
-  
-  //for the userInfo
-  //key contains the key of the data, value has the updated value for the data
-  function updateState(key, value){
-    if(key==='name'||key==='age'||key==='gender'||key==='occupation')
-    {
-      updateStringInfo(key, value);
-    }
-    else
-    {
-      updateListInfo(key, value);
-    }
-  }
-  
-  chrome.runtime.onMessage.addListener(function(response, sendResponse) {
-      console.log(response)
+  chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
+      switch (response.message){
+        case "get_dataCollectionAutorizedBool": //If the popup UI asks for the value of dataCollectionAutorizedBool, return it
+          sendResponse({message: dataCollectionAutorizedBool});
+          break;
+        case "on":  //If the user enabled data Collection
+          dataCollectionAutorizedBool = true;
+          break;
+        case "off": //If the user disabled data Collection
+          dataCollectionAutorizedBool = false;
+          break;
+        default:
+          break;
+      }
   })
   
-  //Add the ad triger, show 1 ad each n minutes
+  //Add the ad triger, show 1 ad each 120 minutes(2hours). 
+  //FIXME make this customizable by the user in future updates.
   chrome.alarms.create("adTriger", {
-    delayInMinutes: 1,
-    periodInMinutes: 1
+    delayInMinutes: 120,
+    periodInMinutes: 120
   });
   
   chrome.alarms.onAlarm.addListener(function(alarm) {
